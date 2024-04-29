125bee9dca56a4255ba0c256d2c00201f7ec62dd
JulienPACTAT <162824988+JulienPACTAT@users.noreply.github.com>
2024-04-29 18:06:54 +0200

    📦 NEW: Add partials WKF and OAuth connection

--- app/(main)/(pages)/layout.tsx
+++ app/(main)/(pages)/layout.tsx
@@ -7,7 +7,7 @@ type TLayoutProps = {
 const Layout = ({ children }: TLayoutProps) => {
   return (
     <div className="border-l-[1px] border-t-[1px] pb-20 h-screen rounded-l-3xl border-muted-foreground/20 overflow-scroll">
-      <div className="w-full">{children}</div>
+      <div className="w-full h-full">{children}</div>
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import { ClerkProvider } from "@clerk/nextjs";
+import ModalProvider from "@/providers/modal-provider";
+import { Toaster } from "sonner";
 
 const font = DM_Sans({ subsets: ["latin"] });
 
@@ -28,7 +30,10 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            {children}
+            <ModalProvider>
+              {children}
+              <Toaster />
+            </ModalProvider>
           </ThemeProvider>
         </ClerkProvider>
       </body>
